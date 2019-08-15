9ee9479b4847258ceba90f7b8cdeaf3c1fb52bbe
hello <hello@163.com>
2019-08-15 17:00:52 +0800

    project start

---
+++ atman/src/components/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+        hello is me
+    </main>
+</template>
+<script>
+export default {
+    name:'Index'
+}
+</script>
+
---
+++ atman/src/components/public/nav.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav>
+        <div class="main">
+            <img src='/static/img/logo.svg'/>
+            <div class="nav">
+                <ul>
+                    <li><router-link to="">首页</router-link></li>
+                    <li><router-link to="">产品与服务</router-link></li>
+                    <li><router-link to="">关于我们 </router-link></li>
+                    <li><router-link to="">帮助 </router-link></li>
+                    <li><router-link to="">合作伙伴</router-link></li>
+                </ul>
+            </div>
+            <div>
+                <span></span>
+                <img src={{imgurl}}/>
+            </div>
+        </div>
+    </nav>
+</template>
+<script>
+export default {
+    name:'Nav',
+    data(){
+        return {
+            imgurl:'/static/img/guojia.svg'
+        }
+    }
+}
+</script>
+
